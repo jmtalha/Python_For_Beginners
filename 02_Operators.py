34d471c9b6cda9fbbf98375a68964c77f12a155b
JMTalha <92794786+jmtalha@users.noreply.github.com>
2021-10-19 22:21:04 +0530

    Operators

---
+++ 02_Operators.py
@@ -0,0 +1,13 @@
+# Python has a concept known as Operators 
+# Addition, Substraction, Multiplication, Division, Modulus, Exponentiation, Floor Division
+
+a = 10
+b = 20
+
+print(a+b)      #Addition   
+print(a-b)      #Substraction
+print(a*b)      #Multiplication
+print(a/b)      #Division
+print(a%b)      #Modulus
+print(a**b)     #Exponentiation
+print(a//b)     #Floor Division
