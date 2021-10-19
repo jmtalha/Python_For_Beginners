d553d969e6412005a8df36d2d719fc176a0ac4f2
JMTalha <92794786+jmtalha@users.noreply.github.com>
2021-10-19 17:50:59 +0530

    Hello World In Python

---
+++ Hello_World.py
@@ -0,0 +1,6 @@
+#This is our First Python Program
+
+print("Hello World!")
+
+# print function is a type function in Python. 
+# Whatever present in print() function that will print as it is in Output 
