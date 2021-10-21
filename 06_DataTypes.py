e0d4281d13b2fe797ea94ee3b83500f5b5ac4dd4
JMTalha <92794786+jmtalha@users.noreply.github.com>
2021-10-21 16:28:30 +0530

    Create 06_DataTypes.py

---
+++ 06_DataTypes.py
@@ -0,0 +1,14 @@
+# A data type is a classfinction of data which tells a compiler or interpreter how the programmer intends to use the data.
+
+# Data type 
+# Integer     int
+# Float       float
+# boolean     bool
+# Character   char
+# String      str
+
+a = 4       # int data type
+b = 4.4     # float data type
+c = True    # bool data type
+d = 'A'     # char data type
+e = "Talha" # str data type
