2d03d30a24140a78a06bc4a69e9cbf350e5e4f6b
JMTalha <92794786+jmtalha@users.noreply.github.com>
2021-10-21 16:22:27 +0530

    Variables

---
+++ 05_Variables.py
@@ -0,0 +1,4 @@
+# A python variable is a reserved memory location to store values. In other words, a variable in a python program gives data to the computer processing.
+# A variable can vary 
+a = 10    # here a is variable, 10 is a data store in variable a
+print(a)
